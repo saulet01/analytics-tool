24f01fd0255d74a98b8dd87bba10920367d61e8d
Saulet Yskak <sauletik01@gmail.com>
2020-01-28 14:49:18 -0600

    First Set Up
    - Installed D3.js
    - Downloaded a sample data to test d3

--- src/components/EdgeBundling.vue
+++ src/components/EdgeBundling.vue
@@ -3,8 +3,24 @@
 </template>
 
 <script>
-    export default {};
+export default {
+    data() {
+        return {
+            loadData: {}
+        };
+    },
+    mounted() {
+        console.log("Data loaded");
+        this.fetchData();
+    },
+    methods: {
+        async fetchData() {
+            let data = await d3.json("./tweeter.json");
+            this.loadData = data;
+            console.log(this.loadData);
+        }
+    }
+};
 </script>
 
-<style>
-</style>
+<style></style>
--- src/main.js
+++ src/main.js
@@ -3,12 +3,15 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import vuetify from './plugins/vuetify';
+import * as d3 from 'd3';
+
+window.d3 = d3;
 
 Vue.config.productionTip = false
 
 new Vue({
-  router,
-  store,
-  vuetify,
-  render: h => h(App)
+    router,
+    store,
+    vuetify,
+    render: h => h(App)
 }).$mount('#app')
